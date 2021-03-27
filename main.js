d2ecd8f757c331e6deb4c08662a7d94f36727364
Make Sure IT Happens <MSIH@users.noreply.github.com>
2021-03-26 20:35:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -90,7 +90,7 @@ Apify.main(async () => {
  //   console.log(dateTo)
 
     const myActors = input.actor ? await getActor(client, [], input.actor) : await getAllActors(client, [], 0)
-
+console.dir(myActors)
    // const myActors = await getAllActors(acts, [], 0)
     console.log(`I have ${myActors.length} actors`)
     for (const myActor of myActors) {
